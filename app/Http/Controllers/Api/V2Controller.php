37108406a68d2356c42af7e07522c37d6bb7fdf2
Administrator <sonvt.vietmmo@gmail.com>
2023-11-10 13:22:08 +0700

    update

--- app/Http/Controllers/Api/V2Controller.php
+++ app/Http/Controllers/Api/V2Controller.php
@@ -14,7 +14,6 @@ class V2Controller extends Controller
 {
     public function getData(){
         $get_method = $this->checkSignSalt($_POST['data']);
-        dd($get_method);
         $data = [];
         if( $get_method['method_name']==="get_app_details"){
             $data = $this->get_app_details();
