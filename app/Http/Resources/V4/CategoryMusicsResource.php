80cca5687a0e794fa07463a3cd768da7a343dee8
Administrator <sonvt.vietmmo@gmail.com>
2023-11-12 14:36:35 +0700

    update

--- app/Http/Resources/V4/CategoryMusicsResource.php
+++ app/Http/Resources/V4/CategoryMusicsResource.php
@@ -18,7 +18,7 @@ class CategoryMusicsResource extends JsonResource
             'category_id' => $this->id,
             'type' => 'category',
             'category_name' => $this->category_name ,
-            'category_image' => $this->category_image ?  asset('storage/sites/'.$this->site_id.'/categories/'.$this->category_image) : asset('storage/default.png') ,
+            'category_image' => $this->category_image ?  asset('storage/domains/'.$this->domain_id.'/categories/'.$this->category_image) : asset('storage/defaultCate.png') ,
         ];
     }
 }
